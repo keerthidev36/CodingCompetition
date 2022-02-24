f99393f147d742480fa29643f5ee079e410bbc9e
keerthi D <keerthidev36@gmail.com>
2022-02-24 20:17:20 +0530

    save

---
+++ src/routes/homeRoutes.js
@@ -0,0 +1,21 @@
+// const express=require("express");
+// const homeRouter=express.Router();
+// function router(nav)
+// {
+//     homeRouter.get('/',function(req,res){
+//     res.render('home',{
+//         nav,
+//         title:'home'
+//     })
+    
+// })
+// homeRouter.post('/new',function(req,res){
+//     res.render('home',{
+//         nav,
+//         title:'home'
+//     })
+// });
+
+// return homeRouter
+// }
+// module.exports=router;
